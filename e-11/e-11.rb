706280aeccdd3a1e9dfac8bbdbe51a20ef1325cd
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-07-28 12:26:24 +0530

    11 final

--- e-11/e-11.rb
+++ e-11/e-11.rb
@@ -1,20 +1,20 @@
 def fact(num)
-  f = 1
+  factorial = 1
   while num > 0 do
-    f *= num
+    factorial *= num
     num -= 1
   end
-  f
+  factorial
 end
 
-def calc_nCr(n,r)
-  (fact(n)/(fact(r)*fact(n-r)))
+def calc_nCr(n, r)
+  (fact(n) / (fact(r) * fact(n - r)))
 end
 
 def pascal_triangle(lines)
   lines.times do |i|
     a = ""
-    (i+1).times do |j|
+    (i + 1).times do |j|
       a += calc_nCr(i,j).to_s + " "
     end
     yield a
@@ -24,11 +24,11 @@ end
 
 #main
 puts "Enter number of lines for pascal triangle"
-lines = gets.chomp
-case lines
+linesToPrint = gets.chomp
+case linesToPrint
   when /^\d+$/
     puts "The Pascal Triangle is:"
-    pascal_triangle(lines.to_i) {|n| puts n}
+    pascal_triangle(linesToPrint.to_i) {|n| puts n}
   else
     puts "You have not entered a valid number"
 end
