5d0965bf60525c7687fe82f904b22d1e85d97136
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-08-09 17:49:10 +0530

    raise inside 'Name' class

--- e-22/bin/e-22.rb
+++ e-22/bin/e-22.rb
@@ -1,19 +1,12 @@
 require_relative '../lib/name'
 
 print "\nEnter first name:"
-first_name = gets.chomp
-
-raise "You haven't entered first name" if first_name.empty?
-raise "First name is not capitalised" if first_name != first_name.capitalize
+first_name = gets.chomp.strip
 
 print "\nEnter last name:"
-last_name = gets.chomp
+last_name = gets.chomp.strip
 
-raise "You haven't entered last name" if last_name.empty?
-
-name = Name.new
-name.first_name = first_name
-name.last_name = last_name
+name = Name.new(first_name, last_name)
 
 puts name
 
--- e-22/lib/name.rb
+++ e-22/lib/name.rb
@@ -1,5 +1,11 @@
 class Name
-  attr_writer :first_name, :last_name
+  def initialize (first_name, last_name)
+    raise "You haven't entered first name" if first_name.empty?
+    raise "First name is not capitalised" if first_name != first_name.capitalize
+    raise "You haven't entered last name" if last_name.empty?
+    @first_name = first_name
+    @last_name = last_name
+  end
   def to_s
     "Hello #{@first_name} #{@last_name}!!!"
   end 
