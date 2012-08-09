31ba5a82dc78f447a9f23a0c516a63c8bea5d48d
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-08-09 23:43:01 +0530

    initialization in block

--- e-20/bin/e-20.rb
+++ e-20/bin/e-20.rb
@@ -6,6 +6,10 @@ principal = gets.chomp.to_f
 print "Enter the Time period(in yrs): "
 time = gets.chomp.to_f
 
-interest = Interest.new(principal, time)
-print "The Difference in amounts is: "
+interest = Interest.new do
+  @p = principal
+  @t = time
+  @r = 10
+end
+print "The difference in amounts is: "
 puts interest.amount_diff
--- e-20/lib/interest.rb
+++ e-20/lib/interest.rb
@@ -1,12 +1,10 @@
 class Interest
-  def initialize(p, t, r = 10)
-    @p = p
-    @t = t
-    @r = r
+  attr_accessor :p, :t, :r
+  def initialize(&block)
+    instance_eval(&block)
   end
-
   def amount_diff
-    calc_amountSI = (@p * @t * @r * 0.01) + @p
+    calc_amountSI = ((@p * @t * @r * 0.01) + @p)
     calc_amountCI = @p * ( (1 + @r * (0.01) ) ** @t )
     (calc_amountCI - calc_amountSI).round(3)
   end
