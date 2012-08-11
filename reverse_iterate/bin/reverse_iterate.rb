7422d9c70e19aa820c0f01256046a0923fec43b9
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-08-11 13:47:02 +0530

    done with each_index, string problem removed

--- reverse_iterate/bin/reverse_iterate.rb
+++ reverse_iterate/bin/reverse_iterate.rb
@@ -1,11 +1,11 @@
 class Array
   def reverse_iterate
-    (self.length-1).step(0, -1) do |i|
-      yield "#{self[i]} "
+    self.each_index do |i|
+      yield self[self.length - (i + 1)]
     end
     puts
   end
 end
 
 #main
-[2, 4, 6, 8].reverse_iterate { |i| print "#{i}" }
+[2, 4, 6, 8].reverse_iterate { |i| print i }
